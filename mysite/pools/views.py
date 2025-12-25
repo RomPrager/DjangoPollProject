e0481eba76d94607e6c99a80d26f8cbde03dc5ac
unknown <rom prager>
2025-12-25 16:50:49 +0200

    final for today

--- mysite/pools/views.py
+++ mysite/pools/views.py
@@ -1,30 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404 , HttpResponseRedirect
 from django.db.models import F
-# Create your views here.
-
-from django.http import HttpResponse
+from django.views import generic
 from django.urls import reverse
 from .models import Question ,Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "pools/index.html", context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "pools/index.html", context)
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "pools/detail.html", {"question": question})
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "pools/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "pools/results.html", {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "pools/results.html", {"question": question})
+
+class IndexView(generic.ListView):
+    template_name = "pools/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "pools/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "pools/results.html"
 
 
 def vote(request, question_id):
