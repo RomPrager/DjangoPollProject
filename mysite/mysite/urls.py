a1592d5f2d4117fe8fc6d96c745d9b90fa1e4ee2
unknown <rom prager>
2025-12-25 14:55:22 +0200

    form

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('pools/', include('pools.urls')),
     path('admin/', admin.site.urls),
+    path('pools/', include('pools.urls')),
+ 
 ]
